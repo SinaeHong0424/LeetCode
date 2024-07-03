e5f236c4a0fab81de1ea10e948e87f9a16ce9903
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-07-02 23:04:10 -0400

    Time: 133 ms (51.75%) | Memory: 51.8 MB (97.48%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cs
@@ -0,0 +1,56 @@
+public class Solution {
+    public void SetZeroes(int[][] matrix) {
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+        bool firstRowHasZero = false;
+        bool firstColHasZero = false;
+
+        // Determine if first row or first column has a zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Use first row and column as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Zero out cells based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out first row if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out first column if needed
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
