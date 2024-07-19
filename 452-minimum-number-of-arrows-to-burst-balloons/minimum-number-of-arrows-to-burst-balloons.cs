aa43995f88164b8c0e5ae123ec3718047bfdb744
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-07-19 08:53:52 -0400

    Time: 563 ms (95.69%) | Memory: 81.1 MB (45.01%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cs
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cs
@@ -1,21 +1,23 @@
 public class Solution {
     public int FindMinArrowShots(int[][] points) {
-        if (points.Length == 0)
+        if (points == null || points.Length == 0) {
             return 0;
+        }
 
+        // Sort balloons by their end coordinate
         Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
 
-        int arrowsShot = 1; 
-        int lastEnd = points[0][1]; 
+        int arrows = 1; // At least one arrow is needed
+        int end = points[0][1]; // Position of the first arrow
 
-        for (int i = 1; i < points.Length; i++) {
-            
-            if (points[i][0] > lastEnd) {
-                arrowsShot++; 
-                lastEnd = points[i][1]; 
+        foreach (var point in points) {
+            if (point[0] > end) {
+                // Need a new arrow for this balloon
+                arrows++;
+                end = point[1];
             }
         }
 
-        return arrowsShot;
+        return arrows;
     }
 }
