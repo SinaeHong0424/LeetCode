23001427b275c4244c12c194a75d1a8d02241339
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-09-11 20:04:01 -0400

    Time: 97 ms (71.88%) | Memory: 46.5 MB (91.67%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cs
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cs
@@ -1,36 +1,37 @@
 public class Solution {
-    private Dictionary<char, string> digitToLetters = new Dictionary<char, string>()
-    {
-        {'2', "abc"},
-        {'3', "def"},
-        {'4', "ghi"},
-        {'5', "jkl"},
-        {'6', "mno"},
-        {'7', "pqrs"},
-        {'8', "tuv"},
-        {'9', "wxyz"}
+    private static readonly Dictionary<char, string> digitToLetters = new Dictionary<char, string>() {
+        { '2', "abc" },
+        { '3', "def" },
+        { '4', "ghi" },
+        { '5', "jkl" },
+        { '6', "mno" },
+        { '7', "pqrs" },
+        { '8', "tuv" },
+        { '9', "wxyz" }
     };
 
     public IList<string> LetterCombinations(string digits) {
         IList<string> result = new List<string>();
-        if (string.IsNullOrEmpty(digits))
+        
+        if (string.IsNullOrEmpty(digits)) {
             return result;
+        }
 
-        Backtrack(digits, 0, new StringBuilder(), result);
+        void Backtrack(int index, string currentCombination) {
+            if (index == digits.Length) {
+                result.Add(currentCombination);
+                return;
+            }
+
+            string possibleLetters = digitToLetters[digits[index]];
+
+            foreach (char letter in possibleLetters) {
+                Backtrack(index + 1, currentCombination + letter);
+            }
+        }
+
+        Backtrack(0, "");
+        
         return result;
-    }
-
-    private void Backtrack(string digits, int index, StringBuilder combination, IList<string> result) {
-        if (index == digits.Length) {
-            result.Add(combination.ToString());
-            return;
-        }
-
-        string letters = digitToLetters[digits[index]];
-        for (int i = 0; i < letters.Length; i++) {
-            combination.Append(letters[i]);
-            Backtrack(digits, index + 1, combination, result);
-            combination.Remove(combination.Length - 1, 1);
-        }
     }
 }
