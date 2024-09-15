d241726d1a9faea45130de7a699a29a287c79c11
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-09-15 10:11:56 -0400

    Time: 87 ms (91.68%) | Memory: 49.1 MB (55.41%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public IList<string> GenerateParenthesis(int n) {
+        IList<string> result = new List<string>();
+        
+        void Backtrack(string current, int open, int close) {
+            if (current.Length == n * 2) {
+                result.Add(current);
+                return;
+            }
+            
+            if (open < n) {
+                Backtrack(current + "(", open + 1, close);
+            }
+            
+            if (close < open) {
+                Backtrack(current + ")", open, close + 1);
+            }
+        }
+        
+        Backtrack("", 0, 0);
+        
+        return result;
+    }
+}
