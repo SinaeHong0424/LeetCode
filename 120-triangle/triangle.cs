209242e1e6eaa3189d48d29c7e0a2354321f6835
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-11-11 22:55:10 -0500

    Time: 2 ms (84.25%) | Memory: 42.2 MB (57.73%) - LeetSync

---
+++ 120-triangle/triangle.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MinimumTotal(IList<IList<int>> triangle){
+        int n=triangle.Count;
+        for(int row= n-2; row >=0; row--){
+            for (int col=0; col <triangle[row].Count; col++){
+                triangle[row][col] +=Math.Min(triangle[row+1][col], triangle[row+1][col+1]);
+            }
+        }return triangle[0][0];
+    }
+}
+
+
