322c8bab1037e3a94e4bb8df6266e7b274b27033
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-11-16 23:21:25 -0500

    Time: 5 ms (61.76%) | Memory: 44.9 MB (56.54%) - LeetSync

--- 72-edit-distance/edit-distance.cs
+++ 72-edit-distance/edit-distance.cs
@@ -1,21 +1,29 @@
 public class Solution {
     public int MinDistance(string word1, string word2) {
-        int[,] dp = new int[word1.Length + 1, word2.Length + 1];
-        for (int i = 0; i <= word1.Length; i++) {
+        int m = word1.Length;
+        int n = word2.Length;
+        int[,] dp = new int[m + 1, n + 1];
+        
+        for (int i = 0; i <= m; i++) {
             dp[i, 0] = i;
         }
-        for (int j = 0; j <= word2.Length; j++) {
+        
+        for (int j = 0; j <= n; j++) {
             dp[0, j] = j;
         }
-        for (int i = 1; i <= word1.Length; i++) {
-            for (int j = 1; j <= word2.Length; j++) {
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (word1[i - 1] == word2[j - 1]) {
                     dp[i, j] = dp[i - 1, j - 1];
                 } else {
-                    dp[i, j] = Math.Min(dp[i - 1, j - 1], Math.Min(dp[i - 1, j], dp[i, j - 1])) + 1;
+                    dp[i, j] = Math.Min(dp[i - 1, j] + 1, 
+                                Math.Min(dp[i, j - 1] + 1, 
+                                         dp[i - 1, j - 1] + 1));
                 }
             }
         }
-        return dp[word1.Length, word2.Length];
+        
+        return dp[m, n];
     }
 }
