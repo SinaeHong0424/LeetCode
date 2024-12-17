8fd1fde06d1893ecd716fc775ccdc08e3016b713
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-12-16 21:44:11 -0500

    Time: 820 ms (14.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH duplicate_tiv_2015 AS (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+),
+unique_location AS (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM duplicate_tiv_2015)
+AND (lat, lon) IN (SELECT lat, lon FROM unique_location);
