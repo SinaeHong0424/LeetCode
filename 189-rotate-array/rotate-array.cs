60d2ebe48aede5a0c41b9b2bbab0b149c9414113
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-12-30 23:07:50 -0500

    Time: 0 ms (100.00%) | Memory: 65 MB (70.42%) - LeetSync

--- 189-rotate-array/rotate-array.cs
+++ 189-rotate-array/rotate-array.cs
@@ -1,12 +1,9 @@
 public class Solution {
     public void Rotate(int[] nums, int k) {
         int n = nums.Length;
-        k = k % n;  
-        
+        k = k % n;
         Reverse(nums, 0, n - 1);
-        
         Reverse(nums, 0, k - 1);
-        
         Reverse(nums, k, n - 1);
     }
 
