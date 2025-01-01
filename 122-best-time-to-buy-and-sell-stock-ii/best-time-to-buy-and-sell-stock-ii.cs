4f85ea85191e3c48f98349197ded0daca850f291
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-01-01 14:13:55 -0500

    Time: 0 ms (100.00%) | Memory: 43.2 MB (20.51%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
@@ -1,13 +1,11 @@
-public class Solution {
-    public int MaxProfit(int[] prices) {
-        int maxProfit = 0;
+public class Solution{
+    public int MaxProfit(int[] prices){
+        int maxProfit=0;
 
-        for (int i = 1; i < prices.Length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
+        for (int i=1; i<prices.Length; i++){
+            if (prices[i]>prices[i-1]){
+                maxProfit +=prices[i]-prices[i-1];
             }
-        }
-
-        return maxProfit;
+        }return maxProfit;
     }
 }
