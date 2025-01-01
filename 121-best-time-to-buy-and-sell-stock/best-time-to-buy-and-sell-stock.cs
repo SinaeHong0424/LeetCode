fd18ebf8aac7d32eecafded83b8d6af14d9ccb62
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2024-12-31 23:55:45 -0500

    Time: 1 ms (100.00%) | Memory: 63.7 MB (6.28%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -1,21 +1,13 @@
-public class Solution {
-    public int MaxProfit(int[] prices) {
-        if (prices.Length == 0) return 0;
-        
-        int minPrice = int.MaxValue;
-        int maxProfit = 0;
-        
-        foreach (int price in prices) {
-            if (price < minPrice) {
-                minPrice = price;
-            } else {
-                int profit = price - minPrice;
-                if (profit > maxProfit) {
-                    maxProfit = profit;
-                }
-            }
-        }
-        
-        return maxProfit;
+public class Solution{
+    public int MaxProfit(int[] prices){
+        if(prices==null || prices.Length==0) return 0;
+
+        int minPrice=prices[0];
+        int maxProfit=0;
+
+        foreach( int price in prices){
+            if(price<minPrice)minPrice=price;
+            else if(price-minPrice > maxProfit)maxProfit=price-minPrice;
+        }return maxProfit;
     }
 }
