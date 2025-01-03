0adb49ed6a25542e081e96ad540cc03f6a1c3ed9
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-01-02 23:38:02 -0500

    Time: 2 ms (62.73%) | Memory: 60.4 MB (79.20%) - LeetSync

--- 55-jump-game/jump-game.cs
+++ 55-jump-game/jump-game.cs
@@ -1,14 +1,9 @@
 public class Solution {
-    public bool CanJump(int[] nums) {
-        int maxReach = 0;
-        
-        for (int i = 0; i < nums.Length; i++) {
-            if (i > maxReach) {
-                return false;
-            }
-            maxReach = Math.Max(maxReach, i + nums[i]);
-        }
-        
-        return maxReach >= nums.Length - 1;
+    public bool CanJump(int[] nums){
+        int farthest=0;
+        for (int i=0; i<nums.Length; i++){
+            if (i >farthest) return false;
+            farthest=Math.Max(farthest,i+nums[i]);
+        }return farthest >=nums.Length-1;
     }
 }
