aab15f67ef7b2ba23ca3117bfaae3793b008a5c3
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-01-03 23:36:28 -0500

    Time: 0 ms (100.00%) | Memory: 45.4 MB (34.49%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cs
+++ 45-jump-game-ii/jump-game-ii.cs
@@ -1,18 +1,15 @@
 public class Solution{
     public int Jump(int[] nums){
+        int n=nums.Length;
+        if (n<=1) return 0;
+
         int jumps=0, currentEnd=0, farthest=0;
-
-        for (int i=0; i<nums.Length-1; i++){
+        for (int i=0; i<n-1; i++){
             farthest=Math.Max(farthest, i+nums[i]);
-
             if(i==currentEnd){
                 jumps++;
                 currentEnd=farthest;
-
-                if(currentEnd >=nums.Length-1)
-                break;
-            }
-        }
-        return jumps;
+            }if(currentEnd>=n-1)break;
+        }return jumps;
     }
 }
