e44e5d521fd586225ca0da1bc9f6abb94cc0faad
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-01-04 22:34:33 -0500

    Time: 5 ms (21.90%) | Memory: 41.6 MB (13.33%) - LeetSync

---
+++ 274-h-index/h-index.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int HIndex(int[] citations) {
+        Array.Sort(citations);
+        
+        int n = citations.Length;
+        
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
+            }
+        }
+        
+        return 0;
+    }
+}
