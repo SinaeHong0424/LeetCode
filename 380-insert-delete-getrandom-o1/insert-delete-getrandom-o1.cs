6edc27dd508d92210e96805d282de601797e2e6a
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-01-05 23:40:48 -0500

    Time: 15 ms (80.02%) | Memory: 117.2 MB (80.80%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cs
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cs
@@ -1,43 +1,52 @@
-public class RandomizedSet {
+using System;
+using System.Collections.Generic;
+
+public class RandomizedSet
+{
     private Dictionary<int, int> dict;
     private List<int> list;
     private Random rand;
 
-    public RandomizedSet() {
+    public RandomizedSet()
+    {
         dict = new Dictionary<int, int>();
         list = new List<int>();
         rand = new Random();
     }
-    
-    public bool Insert(int val) {
-        if (dict.ContainsKey(val)) {
+
+    public bool Insert(int val)
+    {
+        if (dict.ContainsKey(val))
+        {
             return false;
         }
         dict[val] = list.Count;
         list.Add(val);
         return true;
     }
-    
-    public bool Remove(int val) {
-        if (!dict.ContainsKey(val)) {
+
+    public bool Remove(int val)
+    {
+        if (!dict.ContainsKey(val))
+        {
             return false;
         }
-        int index = dict[val];
         int lastElement = list[list.Count - 1];
-        
-        list[index] = lastElement;
-        dict[lastElement] = index;
-        
+        int idx = dict[val];
+        list[idx] = lastElement;
+        dict[lastElement] = idx;
         list.RemoveAt(list.Count - 1);
         dict.Remove(val);
         return true;
     }
-    
-    public int GetRandom() {
+
+    public int GetRandom()
+    {
         int randomIndex = rand.Next(list.Count);
         return list[randomIndex];
     }
 }
+
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
