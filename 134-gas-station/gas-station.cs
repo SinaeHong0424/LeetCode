5f60452c22d0d6942bba880d24879b50443a62e4
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-01-08 22:37:32 -0500

    Time: 1 ms (89.02%) | Memory: 70.1 MB (42.07%) - LeetSync

--- 134-gas-station/gas-station.cs
+++ 134-gas-station/gas-station.cs
@@ -1,21 +1,17 @@
-public class Solution {
-    public int CanCompleteCircuit(int[] gas, int[] cost) {
-        int n = gas.Length;
-        int totalGas = 0, totalCost = 0;
-        int start = 0;
-        int tank = 0;
-
-        for (int i = 0; i < n; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-            tank += gas[i] - cost[i];
-
-            if (tank < 0) {
-                start = i + 1;
-                tank = 0;
+public class Solution{
+    public int CanCompleteCircuit(int[] gas, int[] cost){
+        int totalGas=0;
+        int totalCost=0;
+        int currentGas=0;
+        int startIndex=0;
+        for (int i=0; i<gas.Length; i++){
+            totalGas+=gas[i];
+            totalCost+=cost[i];
+            currentGas+=gas[i]-cost[i];
+            if(currentGas <0){
+                startIndex=i+1;
+                currentGas=0;
             }
-        }
-
-        return totalGas < totalCost ? -1 : start;
+        }return totalGas >= totalCost? startIndex:-1;
     }
 }
