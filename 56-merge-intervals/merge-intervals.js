09de0e1ed841ebae98b53e173ef37b19acfc98f4
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-02-17 12:44:13 -0500

    Time: 11 ms (42.61%) | Memory: 57.8 MB (89.06%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intervals.length===0) return [];
+    intervals.sort((a,b)=>a[0]-b[0]);
+    const merged=[];
+    for(const interval of intervals){
+        if(merged.length===0|| merged[merged.length-1][1]<interval[0]){merged.push(interval);}
+        else{merged[merged.length-1][1]=Math.max(merged[merged.length-1][1],interval[1]);}
+    }return merged;
+};
