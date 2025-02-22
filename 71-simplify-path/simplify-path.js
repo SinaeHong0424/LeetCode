c790a4bb7aef9c7b57b86e79b18108b2e2e2d72b
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-02-21 23:52:44 -0500

    Time: 3 ms (55.79%) | Memory: 54.8 MB (8.19%) - LeetSync

---
+++ 71-simplify-path/simplify-path.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    let stack = [];
+    let parts = path.split('/');
+    
+    for (let part of parts) {
+        if (part === '' || part === '.') {
+            continue;
+        } else if (part === '..') {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            stack.push(part);
+        }
+    }
+    
+    return '/' + stack.join('/');
+};
