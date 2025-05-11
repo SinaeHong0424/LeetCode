f8c6383b4589124060b0409231bdefd0a5b67f9f
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-05-10 22:21:02 -0400

    Time: 3 ms (45.98%) | Memory: 64.3 MB (22.01%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum=nums[0], currentSum=nums[0];
+    for (let i=1; i<nums.length; i++){
+        currentSum=Math.max(nums[i], currentSum+nums[i]);
+        maxSum=Math.max(maxSum,currentSum);
+    }
+    return maxSum;
+};
