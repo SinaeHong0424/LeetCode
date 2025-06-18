fe3c1ec4d43343cfce014e06aaa08dabc277b565
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-06-18 12:06:22 -0400

    Time: 4 ms (43.64%) | Memory: 65.1 MB (40.61%) - LeetSync

---
+++ 89-gray-code/gray-code.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+    const result=[0];
+    for (let i=0; i<n; i++){
+        const size=result.length;
+        for (let j=size-1; j>=0; j--){
+            result.push(result[j] | (1 <<i));
+        }
+    }
+    return result;
+};
