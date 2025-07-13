bad88d657829de50e7a04aa38547fb4147ddb1af
SinaeHong0424 <146133612+SinaeHong0424@users.noreply.github.com>
2025-07-13 16:13:20 -0400

    Time: 0 ms (100.00%) | Memory: 53.3 MB (73.29%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    const row=[1];
+    for (let i=1; i<=rowIndex; i++){row[i]=row[i-1]*(rowIndex-i+1)/i;}
+    return row;
+};
